82b343c90d9d20d41fb2baf7cfce18cbb65f47cc
Vangelis Mazis <mazhs1992@hotmail.com>
2023-07-11 01:35:56 +0300

    route

--- components/cover-image.js
+++ components/cover-image.js
@@ -17,7 +17,7 @@ export default function CoverImage({ title, url, slug }) {
   return (
     <div className="sm:mx-0">
       {slug ? (
-        <Link href={slug} aria-label={title}>
+        <Link href={`posts/${slug}`} aria-label={title}>
           {image}
         </Link>
       ) : (
